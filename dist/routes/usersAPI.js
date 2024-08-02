f74f6fead422e023c7f3ee5c233c93616278d9ef
annahico <annahico@gmail.com>
2024-08-02 10:03:48 +0200

    connected to the server

--- dist/routes/usersAPI.js
+++ dist/routes/usersAPI.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.auth = auth;
 exports.getAuth = getAuth;
 var dotenv_1 = __importDefault(require("dotenv"));
+var express_1 = require("express");
 var jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 var logger_1 = require("../helpers/logger");
 dotenv_1.default.config();
@@ -36,7 +37,6 @@ function getAuth(req, res, next) {
         var jwtSecret = process.env.jwtSecret || '';
         var tokenHeader = req.headers['x-auth-token'];
         if (tokenHeader) {
-            // Asegúrate de que tokenHeader sea una cadena
             var token = Array.isArray(tokenHeader) ? tokenHeader[0] : tokenHeader;
             if (typeof token === 'string') {
                 var decoded = jsonwebtoken_1.default.verify(token, jwtSecret);
@@ -50,4 +50,29 @@ function getAuth(req, res, next) {
         next();
     }
 }
+// Creación de las rutas de la API
+var apiUserRoutes = (0, express_1.Router)();
+// Ruta pública (sin autenticación)
+apiUserRoutes.get('/public', function (req, res) {
+    res.send('This is a public route');
+});
+// Ruta protegida (requiere autenticación)
+apiUserRoutes.get('/protected', auth, function (req, res) {
+    res.send('This is a protected route, you are authenticated!');
+});
+// Ruta semi-protegida (opcionalmente autenticada)
+apiUserRoutes.get('/semi-protected', getAuth, function (req, res) {
+    if (req.token) {
+        res.send('You are authenticated, access granted to semi-protected content');
+    }
+    else {
+        res.send('You are not authenticated, but you can still access this route');
+    }
+});
+// Ejemplo de manejo de errores
+apiUserRoutes.use(function (err, req, res) {
+    logger_1.logger.error("Unexpected error: ".concat(err.message));
+    res.status(500).send('Internal server error');
+});
+exports.default = apiUserRoutes;
 //# sourceMappingURL=usersAPI.js.map
